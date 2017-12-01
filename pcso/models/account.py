dd3e60c7857b896a5bba78a77adc16114db64cbe
Samuel <samuel.salvador.jr16@gmail.com>
2017-12-01 13:57:49 +0800

    [CHANGE] Add new Column for Custom Checking the Price Included, in tax, [CHANGE] Report D. Voucher

--- pcso/models/account.py
+++ pcso/models/account.py
@@ -4,11 +4,12 @@ class AccountTax(models.Model):
 	_inherit = 'account.tax'
 
 	amount_type = fields.Selection(selection=[('group', 'Group of Taxes'), ('fixed', 'Fixed'), ('percent', 'Percentage of Price'), ('division', 'Percentage of Price Tax Included'), ('base_price','Percentage of Base Price - Custom')])
+	is_custom_price_included = fields.Boolean(string='Custom Price Included', default=False)
 
 	def _compute_amount(self, base_amount, price_unit, quantity=1.0, product=None, partner=None):
 		res = super(AccountTax, self)._compute_amount(base_amount, price_unit, quantity, product, partner)
 		if self.amount_type == 'base_price':
-			if self.price_include == True:
+			if self.is_custom_price_included == True:
 				base_amount = price_unit / 1.12
 				return (base_amount * (self.amount/100) ) * -1
 			else:
